b8017224b7af1194cbc56966cd306735ea80913a
Ken <khwken@gmail.com>
2016-07-26 18:26:04 -0700

    updates!

--- client/js/agency.js
+++ client/js/agency.js
@@ -34,12 +34,22 @@
         }
     });
 
+    if ($(".navbar-custom").hasClass("affix")){
+        $(".navbar-brand").attr("src", "https://s3-us-west-1.amazonaws.com/urbantemple/logo/logo_main_wht.png");
+        $("nav li a").css("color", "#fff");
+    }
     $('#mainNav').on( 'affixed-top.bs.affix', function () {
         console.log('unaff');
+        $(".navbar-brand").attr("src", "https://s3-us-west-1.amazonaws.com/urbantemple/logo/logo_main_blk.png");
+        $("nav li a").css("color", "#000");
     });
 
     $('#mainNav').on( 'affix.bs.affix', function () {
         console.log('aff');
+        $(".navbar-brand").attr("src", "https://s3-us-west-1.amazonaws.com/urbantemple/logo/logo_main_wht.png");
+        $("nav li a").css("color", "#fff");
     });
+
+
 
 })(jQuery); // End of use strict
